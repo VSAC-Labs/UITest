d69317ed2f57a303a986992d0d9ce2ca894651e6
Francisco Arguedas <farguedas99@gmail.com>
2023-01-05 14:27:21 -0600

    Test sample working

---
+++ Sample/Sample/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Sample.ViewModels;
+
+namespace Sample.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
